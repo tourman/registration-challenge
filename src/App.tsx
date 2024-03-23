e6bdcc97cb6d85d8e3cb9c23dada880256153a91
tourman <8002615@gmail.com>
2024-03-23 15:48:46 +0000

    ui wrapper: App

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentType, ReactElement } from 'react';
-
-const App: ComponentType<{ children: ReactElement }> = function App({
-  children,
-}) {
-  return children;
-};
-
-export default App;
---
+++ src/App/index.tsx
@@ -0,0 +1,18 @@
+import { ComponentType, ReactElement } from 'react';
+import { Container, Header, Segment } from 'semantic-ui-react';
+import './styles.css';
+
+const App: ComponentType<{ children: ReactElement }> = function App({
+  children,
+}) {
+  return (
+    <>
+      <Header as="h1" content="Registration Challenge" textAlign="center" />
+      <Container text>
+        <Segment>{children}</Segment>
+      </Container>
+    </>
+  );
+};
+
+export default App;
